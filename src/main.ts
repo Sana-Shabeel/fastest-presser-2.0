7a76455ccf0b00042007f0d3a0bb4f3b3f42408f
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-13 13:53:08 +0300

    Add keypress functionality

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,29 @@
 const sKey = document.getElementById("key-s") as HTMLDivElement | null;
+const kKey = document.getElementById("key-k") as HTMLDivElement | null;
+const displayS = document.getElementById("display-s") as HTMLDivElement | null;
+const displayK = document.getElementById("display-k") as HTMLDivElement | null;
 
-sKey?.addEventListener("click", () => {
-  console.log("clicked s");
-});
+// tally
+
+let sCounter = 0;
+let kCounter = 0;
+
+function pressS(e: KeyboardEvent) {
+  if (e.code === "KeyS") {
+    sCounter += 1;
+
+    displayS && (displayS.textContent = `${sCounter}`);
+  }
+}
+function pressK(e: KeyboardEvent) {
+  if (e.code === "KeyK") {
+    kCounter += 1;
+
+    displayK && (displayK.textContent = `${kCounter}`);
+  }
+}
+
+window.addEventListener("keypress", pressS);
+window.addEventListener("keypress", pressK);
 
 export {};
