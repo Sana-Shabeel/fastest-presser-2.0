1b28d0d36aed558deac1c761904908ce18ccc0a3
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-13 15:25:44 +0300

    Add timer functionality

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ const sKey = document.getElementById("key-s") as HTMLDivElement | null;
 const kKey = document.getElementById("key-k") as HTMLDivElement | null;
 const displayS = document.getElementById("display-s") as HTMLDivElement | null;
 const displayK = document.getElementById("display-k") as HTMLDivElement | null;
+const displayTimer = document.getElementById(
+  "display-timer"
+) as HTMLDivElement | null;
+const startBtn = document.getElementById(
+  "start-btn"
+) as HTMLButtonElement | null;
+const newGameBtn = document.getElementById(
+  "new-game"
+) as HTMLButtonElement | null;
+const inputTimer = document.getElementById(
+  "input-timer"
+) as HTMLInputElement | null;
 
 // tally
-
 let sCounter = 0;
 let kCounter = 0;
 
@@ -20,10 +31,52 @@ function pressK(e: KeyboardEvent) {
     kCounter += 1;
 
     displayK && (displayK.textContent = `${kCounter}`);
+    kKey?.classList.toggle("box-shadow");
+
+    // kKey && (kKey.style.boxShadow = "1px 1px 0px 25px rgba(255,216,22,0.28)");
   }
 }
 
-window.addEventListener("keypress", pressS);
-window.addEventListener("keypress", pressK);
+const startGame = (delay: string | undefined) => {
+  window.addEventListener("keypress", pressS);
+  window.addEventListener("keypress", pressK);
+
+  // THE INPUT VALUE COULD BE UNDEFINED
+  let delayTime = parseInt(delay || "0") * 10;
+
+  const interval = setInterval(() => {
+    displayTimer && (displayTimer.textContent = `${delayTime--}`);
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+    window.removeEventListener("keypress", pressS);
+    window.removeEventListener("keypress", pressK);
+
+    // STYLE AND DISPLAY WINNER
+    if (sCounter > kCounter) {
+      displayTimer && (displayTimer.textContent = `S WINS 🎉`);
+      displayTimer && (displayTimer.style.backgroundColor = "#3b82f6");
+    } else if (sCounter < kCounter) {
+      displayTimer && (displayTimer.textContent = `K WINS 🎉`);
+      displayTimer && (displayTimer.style.backgroundColor = "#fde047");
+    } else {
+      displayTimer && (displayTimer.textContent = `NO ONE WINS`);
+    }
+  }, parseInt(delay || "0") * 1000);
+};
+
+startBtn?.addEventListener("click", () => {
+  startGame(inputTimer?.value);
+});
+
+newGameBtn?.addEventListener("click", () => {
+  displayTimer && (displayTimer.style.backgroundColor = "#1e293b");
+  displayTimer && (displayTimer.textContent = "000");
+  sCounter = 0;
+  kCounter = 0;
+  displayS && (displayS.textContent = `0`);
+  displayK && (displayK.textContent = `0`);
+});
 
 export {};
