8b425a8047325f76b6159d243a060de176834bda
Sana_Shabeel <sanastrobarre99@gmail.com>
2023-02-13 07:04:35 +0300

    Add font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        NotoSans: "'Noto Sans Syloti Nagri', sans-serif",
+      },
+    },
   },
   plugins: [],
 };
